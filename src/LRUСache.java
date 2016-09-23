3dceb3a892c53d5d24f7f6a81793d0f6be2e8276
timur <gamatima@yandex.ru>
2016-09-24 00:57:30 +0300

    list insert optimization

--- src/LRUСache.java
+++ src/LRUСache.java
@@ -1,36 +1,96 @@
 import java.util.*;
 
 public class LRUСache<K, V> {
-    LinkedList<K> timeList;
-    HashMap<K, V> cache;
-    int size;
+    HashMap<K, Node> cache;
+    private Node fakeHead, fakeTail;
+    private int size, maxSize;
 
     public LRUСache(int size) {
-        this.size = size;
-        timeList = new LinkedList<>();
+        this.maxSize = size;
+        this.size = 0;
+        fakeTail = new Node(null, null, null, null);
+        fakeHead = new Node(null, null, null, fakeTail);
         cache = new HashMap<>(size);
     }
 
     private void up(K key) {
-        timeList.remove(key);
-        timeList.addFirst(key);
+        Node node = cache.get(key);
+        node.delete();
+        node.setNeighbors(fakeHead, fakeHead.getNext());
     }
 
     public void put(K key, V value) {
-        if (timeList.size() == size) {
-            K deleteKey = timeList.peekLast();
-            cache.remove(deleteKey);
+        size++;
+        if (size > maxSize) {
+            cutTail();
         }
-        cache.put(key, value);
-        timeList.addFirst(key);
+        cache.put(key, new Node(key, value, fakeHead, fakeHead.getNext()));
     }
 
     public Optional<V> get(K key) {
         if (cache.containsKey(key)) {
             up(key);
-            return Optional.of(cache.get(key));
+            return Optional.of(cache.get(key).getValue());
         } else {
             return Optional.empty();
         }
     }
+
+    private void remove(K key) {
+        cache.get(key).delete();
+        cache.remove(key);
+    }
+
+    private void cutTail() {
+        remove(fakeTail.getPrev().getKey());
+    }
+
+    private class Node {
+        private K key;
+        private V value;
+        private Node next, prev;
+
+        public Node(K key, V value, Node prev, Node next) {
+            this.key = key;
+            this.value = value;
+            setNeighbors(prev, next);
+        }
+
+        public void setNeighbors(Node prev, Node next) {
+            this.next = next;
+            this.prev = prev;
+            if (this.prev != null) {
+                this.prev.next = this;
+            }
+            if (this.next != null) {
+                this.next.prev = this;
+            }
+        }
+
+        public V getValue() {
+            return value;
+        }
+
+        public Node getNext() {
+            return next;
+        }
+
+        public Node getPrev() {
+            return prev;
+        }
+
+        public K getKey() {
+            return key;
+        }
+
+        public void delete() {
+            if (this.prev != null) {
+                this.prev.next = this.next;
+            }
+            if (this.next != null) {
+                this.next.prev = this.next;
+            }
+        }
+    }
+
 }
